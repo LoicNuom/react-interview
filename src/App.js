85e72b2e6eec4d5e8a066138e02664ffa83bd87b
LoicNuom <loic@nuom.co.uk>
2020-09-16 15:23:25 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import cx from 'classnames'
+import {Home, Article} from "Pages"
+
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <div className={cx("container")}>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/article/:id">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
